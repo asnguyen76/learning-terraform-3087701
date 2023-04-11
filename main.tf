a7f643c3111b8de019048b90090e3bdfbe687b86
Anthony Nguyen <99445643+asnguyen76@users.noreply.github.com>
2023-04-11 11:21:04 -0400

    create security group

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   vpc_security_group_ids = [aws_security_group.blog.id]
-  
+
   tags = {
     Name = "HelloWorld"
   }
@@ -40,7 +40,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0]
+  cidr_blocks = ["0.0.0.0/0"]
 
   aws_security_group_id = aws_security_group.blog.id
 }
@@ -50,7 +50,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0]
+  cidr_blocks = ["0.0.0.0/0"]
 
   aws_security_group_id = aws_security_group.blog.id
 }
@@ -60,7 +60,7 @@ resource "aws_security_group_rule" "blog_everything_out" {
   from_port   = 0
   to_port     = 0
   protocol    = -1
-  cidr_blocks = ["0.0.0.0/0]
+  cidr_blocks = ["0.0.0.0/0"]
 
   aws_security_group_id = aws_security_group.blog.id
 }
