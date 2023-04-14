b1f0a9aeec3e0649a4666c6cfe270b24ab599354
Anthony Nguyen <99445643+asnguyen76@users.noreply.github.com>
2023-04-14 17:04:22 -0400

    fix variables

--- variables.tf
+++ variables.tf
@@ -6,37 +6,36 @@ variable "instance_type" {
 variable "ami_filter" {
   description = "Name filter and owner for AMI"
 
-  type = object({
-    name = string
+  type    = object ({
+    name  = string
     owner = string
   })
 
   default = {
-    values = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
-    owners = "979382823631" # Bitnami
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
   }
 }
 
 variable "environment" {
-  description = "Development Environment"
-  
-  type = object ({
-    name            = string
+  description = "Deployment environment"
+
+  type        = object ({
+    name           = string
     network_prefix = string
   })
-
   default = {
-    name = "dev"
+    name           = "dev"
     network_prefix = "10.0"
   }
 }
 
-variable "asg_min_size" {
-  description = "Minimum number of instances in the ASG"
+variable "asg_min" {
+  description = "Minimum instance count for the ASG"
   default     = 1
 }
 
-variable "asg_max_size" {
-  description = "Maximum number of instances in the ASG"
+variable "asg_max" {
+  description = "Maximum instance count for the ASG"
   default     = 2
 }
