c362f351b7aa44aab19baa87cccc3a5c0b71a7d4
Anthony Nguyen <99445643+asnguyen76@users.noreply.github.com>
2023-04-17 18:24:29 -0400

    edit main

--- qa/main.tf
+++ qa/main.tf
@@ -2,10 +2,10 @@ module "qa" {
     source = "../modules/blog"
 
     environment = {
-        name            = "qa"
+        name           = "qa"
         network_prefix = "10.1"
     }
 
-    asg min_size = 1
-    asg max_size = 1
+    asg min_size = 0
+    asg max_size = 0
 }
